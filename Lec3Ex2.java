c343ef9285465dee3ab7e84e9debd19b60c00799
baramon0418 <wnjswndus@naver.com>
2024-03-19 11:26:05 +0900

    3주차java

---
+++ Lec3Ex2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Lec3Ex2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		int num;
+		
+
+		System.out.print("양의 정수를 입력하세요:");
+		num = input.nextInt();
+		
+		for(int i =0; i<=num; i++) {
+			System.out.println(num+"의 약수는 다음과 같습니다.");
+		}
+
+	}
+
+}
---
+++ MultipleOfThree.java
@@ -0,0 +1,9 @@
+//입력받은수가3의배수인지판별하는프로그램을작성하시오
+public class MultipleOfThree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
