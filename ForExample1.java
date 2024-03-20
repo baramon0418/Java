2dff85a8e3352aa99bbb5b27ba177408fca1731e
baramon0418 <108270144+baramon0418@users.noreply.github.com>
2024-03-20 10:19:20 +0900

    Add files via upload

---
+++ ForExample1.java
@@ -0,0 +1,14 @@
+
+public class ForExample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		for (int i = 0; i <= 10; i++) {
+			System.out.println(sum += i);
+		}
+		
+
+	}
+
+}
