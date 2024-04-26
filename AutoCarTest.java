5e33412573422b91403207808700010c78dceca4
baramon0418 <108270144+baramon0418@users.noreply.github.com>
2024-04-26 10:07:32 +0900

    Add files via upload

---
+++ AutoCarTest.java
@@ -0,0 +1,14 @@
+
+public class AutoCarTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AutoCar obj = new AutoCar();
+		//OperateCar obj = new AutoCar();
+		obj.start();
+		obj.stop();
+		obj.setSpeed(10);
+		obj.turn(20);
+	}
+
+}
