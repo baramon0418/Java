c3b4809e18c9724f718e0f2b6a867fb589808aed
baramon0418 <108270144+baramon0418@users.noreply.github.com>
2024-05-18 14:14:42 +0900

    Add files via upload

--- ForExample1.java
+++ ForExample1.java
@@ -1,14 +1,14 @@
-
+//1부터 10까지의 정수의 합을 계산
 public class ForExample1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int sum = 0;
-		for (int i = 0; i <= 10; i++) {
-			System.out.println(sum += i);
+		for (int i = 1; i <= 10; i++) {
+			//System.out.println(sum += i); 이러면 더 하는 과정도 다 출력됨
+			sum += i;			
 		}
-		
-
+		System.out.printf("1부터 10까지의 정수의 합 = %d", sum);
 	}
 
 }
