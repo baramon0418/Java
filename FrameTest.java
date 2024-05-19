b19b83c57551cc105c5c5de2e3237f758df51ca3
baramon0418 <108270144+baramon0418@users.noreply.github.com>
2024-05-19 23:02:45 +0900

    Add files via upload

---
+++ FrameTest.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+public class FrameTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyFrame f = new MyFrame("Frame Test"); //스윙에서 만든 프레임
+		
+	}
+
+}
