5b47e2ddb3a3d5422ffcf025b347c2e502ecfe16
baramon0418 <108270144+baramon0418@users.noreply.github.com>
2024-05-28 08:36:46 +0900

    Add files via upload

--- Lec2Ex1.java
+++ Lec2Ex1.java
@@ -16,7 +16,7 @@ public class Lec2Ex1 {
 		if(num1>num2) {
 			System.out.print("첫번째 숫자가 큽니다.");
 		}
-		if(num2>num1){
+		else if(num2>num1){
 			System.out.print("두번째 숫자가 큽니다.");
 		}
 		else {
@@ -26,3 +26,31 @@ public class Lec2Ex1 {
 	}
 
 }
+
+/** 성적에 따라 학점 결정하기
+ * import java.util.Scanner;
+
+public class Grade {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("성적을 입력하시오: ");
+        int score = sc.nextInt();
+        char grade;
+
+        if (score >= 90) {
+            grade = 'A';
+        } else if (score >= 80) {
+            grade = 'B';
+        } else if (score >= 70) {
+            grade = 'C';
+        } else {
+            grade = 'F';
+        }
+
+        System.out.println("학점 " + grade);
+
+        sc.close();
+    }
+}
+/
+ */
