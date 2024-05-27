ade56396d47af99794278bc8fd538821c51e97a7
baramon0418 <108270144+baramon0418@users.noreply.github.com>
2024-05-27 11:08:15 +0900

    Add files via upload

--- LeapYear.java
+++ LeapYear.java
@@ -19,3 +19,25 @@ public class LeapYear {
 	}
 
 }
+
+/** 
+ * import java.util.Scanner;
+
+	public class LeapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("연도를 입력하세요: ");
+        int year = sc.nextInt();
+        boolean isLeapYear = false;
+
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            isLeapYear = true;
+        }
+
+        System.out.println(year + "년은 윤년인가요? " + isLeapYear);
+
+        sc.close();
+    }
+}
+
+ * */
