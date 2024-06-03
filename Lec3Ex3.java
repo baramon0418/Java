781b9314d30939421dd1d19b6a21311e2bf8619c
baramon0418 <108270144+baramon0418@users.noreply.github.com>
2024-06-04 05:18:15 +0900

    Add files via upload

--- Lec3Ex3.java
+++ Lec3Ex3.java
@@ -1,11 +1,11 @@
-
+//1과 1000사이의 정수 중 소수를 출력하시오
 public class Lec3Ex3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int count = 0;
 		
-		for(int i=2; i<=1000; i++) {
+		for(int i=1; i<=1000; i++) {
 			int j;
 			for(j=2; j<i; j++) {
 				count++;
