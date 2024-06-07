96b4242aa79337420e1ad96a96abf79d6ce4dfc5
baramon0418 <108270144+baramon0418@users.noreply.github.com>
2024-06-08 00:01:32 +0900

    Add files via upload

---
+++ ManagerTest.java
@@ -0,0 +1,11 @@
+//실습3 직원(Employee)과 매니저(Manager)의예
+public class ManagerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Manager m = new Manager("Tom", "Seoul", 1000000, 123456, 50000);
+		System.out.println(m);
+		m.test();
+	}
+
+}
