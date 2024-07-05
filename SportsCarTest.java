ed40ae877bf0eeddcb02893fbdad858694e35921
baramon0418 <108270144+baramon0418@users.noreply.github.com>
2024-07-06 07:29:38 +0900

    Add files via upload

---
+++ SportsCarTest.java
@@ -0,0 +1,10 @@
+
+public class SportsCarTest {
+	public static void main(String[] args) {
+		Car obj1 = new SportsCar();
+		obj1.setColor("red");
+		
+		//obj1.print(); // Car.print(); 객체 선언 안해도 쓸 수 있음 정적메소드
+		//Car.color="red"
+	}
+}
