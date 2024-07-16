c631394fc17d10b79036d501d998eb995420d295
baramon0418 <108270144+baramon0418@users.noreply.github.com>
2024-07-16 13:56:58 +0900

    Add files via upload

--- Television.java
+++ Television.java
@@ -1,8 +1,30 @@
 
 public class Television {
-	private int channel; //채널 번호
-	int volume; //볼륨
-	boolean onOff; //전원 상태
+	private int channel = 7; //채널 번호, 7로 초기화
+	private int volume; //볼륨
+	private boolean onOff; //전원 상태
+	
+	public Television() {
+		this(1,1,false);
+//		channel = 1;
+//		volume = 1;
+//		onOff = false;
+	}
+	
+	public Television(int c) {
+		this(c,1,false);
+	}
+	
+	public Television(int c, int v) {
+		this(c,v,false);
+	}
+	
+	//생성자 (int c, int v, boolean o)
+	public Television(int c, int v, boolean o) {
+		this.channel = c;
+		this.volume = v;
+		onOff = o;
+	}
 
 	void print() {
 		System.out.println("텔레비전의 채널은 " + channel + "이고 볼륨은 " + volume + "입니다.");
@@ -16,5 +38,7 @@ public class Television {
 		channel = newChannel;
 		if(newChannel < 1) newChannel = 7;
 	}
+
+
 		
 }
